3f79af144c1e04edb1931a5a1d5ac1b7936f0482
randha11 <ayisharandha11@gmail.com>
2024-10-07 09:08:04 +0000

    Add files via upload

---
+++ marklist.py
@@ -0,0 +1,10 @@
+print("enter mark of five subject:")
+s1=int(input("msrk1:"))
+s2=int(input("msrk2:"))
+s3=int(input("msrk3:"))
+s4=int(input("msrk4:"))
+s5=int(input("msrk5:"))
+total=s1+s2+s3+s4+s5
+print("total mark:",total)
+percentage=(total/500.0)*100
+print("percentage:",percentage)
