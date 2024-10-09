9e7ab2c5ec909bfa188576a7a04dd11b28938a04
randha11 <ayisharandha11@gmail.com>
2024-10-09 11:02:11 +0530

    Add files via upload

---
+++ week2/largest_of_three_numbers.py
@@ -0,0 +1,11 @@
+a=int(input("enter first number : "))
+b=int(input("enter second number : "))
+c=int(input("enter third number : "))
+if (a>b and a>c):
+    print("a is the largest.",a)
+elif (b>a and b>c):
+    print("b is the largest.",b)
+else:
+    print("c is the largest.",c)
+           
+        
